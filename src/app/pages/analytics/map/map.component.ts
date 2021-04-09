7fe2a950a4b17483dcdd1979895556e4ebb22373
Krunal Vasoya <krunalvasoya@gmail.com>
2021-04-09 18:27:47 +0530

    added diseases mapping in Map tab

--- src/app/pages/analytics/map/map.component.ts
+++ src/app/pages/analytics/map/map.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements OnInit {
-  url = "https://public.tableau.com/views/Sehat-IntelVisualization/MainParametersDistribution?:language=en&:display_count=y&:toolbar=n&:origin=viz_share_link"
+  url = "https://public.tableau.com/views/Sehat-IntelDiseasesMapping/Dashboard1?:language=en&:display_count=y&:toolbar=n&:origin=viz_share_link"
   constructor() {
     // this.reload();
   }
